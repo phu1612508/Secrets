4c7ff775579d0fd165a50696ee5a4c2e0a3f9638
phu1612508 <43996827+phu1612508@users.noreply.github.com>
2020-08-29 09:01:25 +0700

    level 3

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodayParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app = express();
 
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
 
 const User = mongoose.model("User", userSchema);
 
@@ -39,7 +40,7 @@ app.route("/login")
 
   .post(function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne(
       {email: req.body.username},
       function(err, foundUser) {
@@ -66,7 +67,7 @@ app.route("/register")
   .post(function(req, res) {
     const newUser = User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
     newUser.save(function(err) {
       if (!err) {
